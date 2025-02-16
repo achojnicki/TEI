45eb401921423b78b869fe9b4c9b2c4e16c86678
Adrian Chojnicki <adrian@chojnicki.dev>
2025-02-16 15:13:19 +0000

    changes

--- converter_simple_grid.py
+++ converter_simple_grid.py
@@ -15,36 +15,32 @@ class converter:
 body {
 	display: block;
 	}
-table {
-	margin: 0px;
-	padding: 0px;
-	border-spacing: 0px;
-}
-tr {
+.img {
+	display: grid;
+	grid-template-columns: repeat(<_image_width_>, 1px);
 	margin: 0px;
 	padding: 0px;
 }
-td {
+.pixel {
 	width: 1px;
 	height: 1px;
-	margin: 0px;
-	padding: 0px;
-}
+ }
 
-</style>
-<table style="border-spacing: 0px;">"""
+</style>"""
 
 	def __init__(self):
 		self._img=Image.open(argv[1])
 		self._bitmap=np.array(self._img)
 		self._output_file='data.html'
+
+		self._data=self._data.replace('<_image_width_>', str(self._img.size[0]))
 		self._convert()
 		self._save()
 
 
 
 	def cod(self,col):
-		return f"""<td style="background-color: {col}"></td>"""
+		return f"""<div class="pixel" style="background:{col}"></div>"""
 
 
 	def col(self,r,g,b):
@@ -52,12 +48,11 @@ td {
 
 
 	def _convert(self):
+		self._data+="""<div style="img">"""
 		for x in tqdm(range(0,self._img.size[1], 1)):
-			self._data+="<tr>"
 			for y in range(0,self._img.size[0], 1):
-					self._data+=self.cod(self.col(self._bitmap[x][y][0],self._bitmap[x][y][1],self._bitmap[x][y][2]))
-			self._data+="</tr>"
-		self._data+="</table></body></html>	"
+				self._data+=self.cod(self.col(self._bitmap[x][y][0],self._bitmap[x][y][1],self._bitmap[x][y][2]))
+		self._data+="</div></body></html>"
 
 	def _save(self):
 		with open(self._output_file,'w') as f:
